b7bb1b83ab5bc6405622115628f92aa0bf58213c
qpiny <github@rejna.org>
2013-04-17 15:47:33 +0200

    Daily commit

--- console/web/src/main/resources/www/console.js
+++ console/web/src/main/resources/www/console.js
@@ -17,36 +17,76 @@ var GraphMgr = {
 
 $(function() {
 	var memoryData = [ {
+		label : "Used memory",
+		data : []
+	}, {
 		label : "Free memory",
 		data : []
-	}, {
-		label : "Total memory",
+	} ];
+
+	var cpuData = [ {
+		label : "CPU load",
 		data : []
 	}, {
-		label : "Max memory",
+		label : "GC load",
 		data : []
 	} ];
-	updateMemoryGraph = function(timestamp, mem) {
-		memoryData[0].data.push([ timestamp, mem[0] ]);
-		memoryData[1].data.push([ timestamp, mem[1] ]);
-		memoryData[2].data.push([ timestamp, mem[2] ]);
+	
+	var previousGC = { ts: 0, load: 0 };
+
+	updateSystemGraph = function(_timestamp, system) {
+		var timestamp = parseInt(_timestamp);
+		var totalMemory = parseInt(system.totalMemory);
+		var freeMemory = parseInt(system.freeMemory);
+
+		memoryData[0].data.push([ timestamp, totalMemory - freeMemory ]);
+		memoryData[1].data.push([ timestamp, freeMemory ]);
 
 		$.plot($('#memory'), memoryData, {
 			series : {
 				stack : true,
 				lines : {
 					show : true,
-					fill : true
-				}
+					fill : true,
+				},
+				shadowSize: 0
 			},
 			xaxis : {
 				mode : "time",
 				position : "bottom",
 				timeformat : "%H:%M:%S",
 				minTickSize : [ 5, "second" ]
+			},
+			legend : {
+				position : "nw"
+			}
+		});
+
+		var cpuLoad = parseInt(system.cpuLoad);
+		var cpuCount = parseInt(system.cpuCount);
+		var gcTime = parseInt(system.gcTime);
+
+		if (cpuLoad >= 0)
+			cpuData[0].data.push([ timestamp, cpuLoad / cpuCount ]);
+		cpuData[1].data.push([ timestamp, (gcTime - previousGC.load) / (timestamp - previousGC.ts) / cpuCount ])
+		previousGC.ts = timestamp;
+		previousGC.load = gcTime;
+		$.plot($('#cpu'), cpuData, {
+			series : {
+				shadowSize : 0
+			},
+			xaxis : {
+				mode : "time",
+				position : "bottom",
+				timeformat : "%H:%M:%S",
+				minTickSize : [ 5, "second" ]
+			},
+			legend : {
+				position : "nw"
 			}
 		});
 	};
+
 	var ws = $.websocket("ws://localhost:8888/websocket/", {
 		open : function() {
 			ws.send('MonitorSubscribe', [ '*' ]);
@@ -54,7 +94,7 @@ $(function() {
 		events : {
 			MonitorData : function(e) {
 				var timestamp = e.data.timestamp;
-				updateMemoryGraph(timestamp, e.data.memory)
+				updateSystemGraph(timestamp, e.data.system)
 				$.each(e.data.counters, function(name, value) {
 					GraphMgr.addValue(name, timestamp, value);
 				});
--- console/web/src/main/scala/org/rejna/solver/console/web/WebSocket.scala
+++ console/web/src/main/scala/org/rejna/solver/console/web/WebSocket.scala
@@ -3,12 +3,13 @@ package org.rejna.solver.console.web
 import scala.util.Random
 import scala.collection.mutable.Set
 import scala.collection.Map
+import scala.collection.JavaConversions._
 import scala.concurrent.duration._
 
-import akka.actor.Actor
+import akka.actor.{ Actor, Cancellable }
 
 import java.nio.channels.ClosedChannelException
-import java.lang.management.ManagementFactory
+import java.lang.management.{ ManagementFactory, OperatingSystemMXBean }
 import java.util.Date
 
 import spray.json._
@@ -26,48 +27,68 @@ sealed abstract class WebSocketMessage
 case object StartComputation extends WebSocketMessage
 case class MonitorSubscribe(filters: String*) extends WebSocketMessage
 case class MonitorUnsubscribe(filters: String*) extends WebSocketMessage
-case class MonitorData(timestamp: Long, counters: Map[String, Long], gauges: Map[String, MonitoredValue], memory: (Long, Long, Long)) extends WebSocketMessage
+case class MonitorData(timestamp: Long, counters: Map[String, Long], gauges: Map[String, MonitoredValue], system: SystemData) extends WebSocketMessage
 
+case class SystemData(val freeMemory: Long, val totalMemory: Long, val maxMemory: Long, val cpuLoad: Double, val cpuCount: Int, val gcTime: Long) {
+
+  def this(runtime: Runtime, osbean: OperatingSystemMXBean) = this(
+    runtime.freeMemory,
+    runtime.totalMemory,
+    runtime.maxMemory,
+    osbean.getSystemLoadAverage,
+    osbean.getAvailableProcessors,
+    (java.lang.management.ManagementFactory.getGarbageCollectorMXBeans :\ 0L)(_.getCollectionTime + _))
+
+  def this() = this(Runtime.getRuntime, ManagementFactory.getOperatingSystemMXBean)
+}
 
 class WebSocketHandler extends Actor {
   val subscribers = Set.empty[WebSocketFrameEvent]
   val monitor = Monitor(DefaultSystem.system)
-  val runtime = Runtime.getRuntime
+  var schedule: Option[Cancellable] = None
 
-  override def preStart = {
-    context.system.scheduler.schedule(2 seconds, 1 seconds, self, SendData)(context.system.dispatcher)
+  def addSubscriber(s: WebSocketFrameEvent): Unit = {
+    subscribers += s
+    if (!schedule.isDefined)
+      schedule = Some(context.system.scheduler.schedule(0 seconds, 1 seconds, self, SendData)(context.system.dispatcher))
   }
-  
-  def getCpuLoad = ManagementFactory.getOperatingSystemMXBean.getSystemLoadAverage
+
+  def removeSubscriber(s: WebSocketFrameEvent): Unit = {
+    subscribers -= s
+    if (subscribers.isEmpty) {
+      schedule map { _.cancel }
+      schedule = None
+    }
+  }
+
   def receive = {
     case WSMessage(wsFrame) =>
       val message = wsFrame.readText
-      println(s"Receive web socket message : ${message}")
       val json = message.asJson
-      println(s"json value => ${json.prettyPrint}")
       json.convertTo[WebSocketMessage] match {
-        case StartComputation => new StandAlone().startup
-        case MonitorSubscribe(filters) => subscribers += wsFrame
-        case MonitorUnsubscribe(filters) => subscribers -= wsFrame
+        case StartComputation =>
+          new StandAlone().startup
+        case MonitorSubscribe(filters) =>
+          addSubscriber(wsFrame)
+        case MonitorUnsubscribe(filters) =>
+          removeSubscriber(wsFrame)
       }
-      
-      println("client registered")
+
     case SendData =>
       val counters = monitor.getCounters
       val gauges = monitor.getGauges
 
-      val data = MonitorData(new Date().getTime, counters, gauges, (runtime.freeMemory, runtime.totalMemory, runtime.maxMemory))
-      lazy val cpuLoad = getCpuLoad
+      val data = MonitorData(new Date().getTime, counters, gauges, new SystemData)
       for (s <- subscribers) {
         try {
           if (!s.channel.isConnected) {
-            subscribers -= s
+            removeSubscriber(s)
           } else {
             s.writeText(data.toJson.compactPrint)
           }
         } catch {
           case e: ClosedChannelException => // never happens, even if we write in closed channel
-            subscribers -= s
+            removeSubscriber(s)
         }
       }
   }
